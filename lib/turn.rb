0964dfad72b3ba645902def9e68ade87fc02f370
ed.a.ezekiel <ed.a.ezekiel@gmail.com>
2018-09-30 15:40:13 +0000

    automatically backed up by learn

--- lib/turn.rb
+++ lib/turn.rb
@@ -11,9 +11,7 @@ def display_board(board)
 end
 
 def valid_move?(board, index)
-#d return true if the index is within the correct range of 0-8 and is currently unoccupied by an X or O token.
-!(position_taken?(board)) && index.between(0, 8)
-
+  index.between?(0,8) && !position_taken?(board, index)
 end
 
 
