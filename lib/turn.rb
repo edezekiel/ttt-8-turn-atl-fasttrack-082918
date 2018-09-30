9cfa5dfe1ba9aac404aab41ed293fd6be7a56a85
ed.a.ezekiel <ed.a.ezekiel@gmail.com>
2018-09-30 15:38:05 +0000

    automatically backed up by learn

--- lib/turn.rb
+++ lib/turn.rb
@@ -3,7 +3,11 @@ def position_taken?(board, index)
 end
 
 def display_board(board)
-
+  puts " #{board[0]} | #{board[1]} | #{board[2]} "
+  puts "-----------"
+  puts " #{board[3]} | #{board[4]} | #{board[5]} "
+  puts "-----------"
+  puts " #{board[6]} | #{board[7]} | #{board[8]} "
 end
 
 def valid_move?(board, index)
@@ -12,6 +16,21 @@ def valid_move?(board, index)
 
 end
 
-def move(board)
 
+
+dedef move(board, index, current_player)
+  if current_player == "X" || current_player == "O"
+  board[index] = current_player
 end
+
+
+
+
+
+
+
+
+
+
+
+index.between(0,8)
